befbc8e193bbd230a9c536d012061ac6e1f9eb7f
trpsl <trpsl755@gmail.com>
2020-06-07 15:14:49 +0200

    payton: Disable fluence

--- properties.mk
+++ properties.mk
@@ -11,9 +11,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.calfile4=/vendor/etc/acdbdata/Hdmi_cal.acdb \
     persist.vendor.audio.calfile5=/vendor/etc/acdbdata/Headset_cal.acdb \
     persist.vendor.audio.calfile6=/vendor/etc/acdbdata/Speaker_cal.acdb \
-    persist.vendor.audio.fluence.speaker=true \
-    persist.vendor.audio.fluence.voicecall=true \
-    persist.vendor.audio.fluence.voicecomm=true \
+    persist.vendor.audio.fluence.speaker=false \
+    persist.vendor.audio.fluence.voicecall=false \
+    persist.vendor.audio.fluence.voicecomm=false \
     persist.vendor.audio.fluence.voicerec=false
 
 # FM
