af6952840d8c602fec02f771129ab7b0469adf09
trpsl <trpsl755@gmail.com>
2020-06-16 03:16:47 +0200

    payton: Decommonize LineageActions

--- device.mk
+++ device.mk
@@ -78,6 +78,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# LineageActions
+PRODUCT_PACKAGES += \
+    LineageActions
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1-service \
